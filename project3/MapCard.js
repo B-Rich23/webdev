d4060315c8aa30a341fc6dcd1d738f3d93f70e60
Brandan Richardson <brandanr@gmail.com>
2018-03-27 15:09:15 -0700

    updated mapHeader.js

--- project3/MapCard.js
+++ project3/MapCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableHighlight } from 'react-native';
 import { Header, ButtonGroup, Button, Avatar, List, ListItem, Card, Input, Icon } from 'react-native-elements';
 
 export default class App extends React.Component {
@@ -20,84 +20,74 @@ updateIndex (selectedIndex) {
   render() {
   const buttons = ['Home', 'Past Quest', 'Next Quest', 'Stats']
   const { selectedIndex } = this.state
-  const list = [
-  {
-    title: 'Steps',
-    value: '2,867'
-  },
-  {
-    title: 'Distance',
-    value: '1 mile'
-  },
-  {
-    title: 'Elevation',
-    value: '1 mile'
-  },
-  {
-    title: 'Time',
-    value: '30 mins'
-  },
-  {
-    title: 'Avg Speed',
-    value: '10 mph'
-  }
-  
-]
     return (
       <View style={styles.container}>
-	     <Card containerStyle={{flexDirection: 'row', width: "100%",
-			height: 80, backgroundColor: 'rgba(49, 111,244, 1)',
-			borderColor: 'rgba(49, 111,244, 1)', justifyContent: 'space-around',
-			alignItems: 'stretch', marginTop: 0
-			}}> 
-		      <Header
-				  statusBarProps={{ barStyle: 'light-content' }}
-				  centerComponent={{ text: 'FitQuest', style: { color: '#fff',
-				  fontWeight: 'bold', fontSize: 20, marginTop: 15 } }}
-				  outerContainerStyles={{ backgroundColor: 'rgba(49, 111,244, 1)',
-				   flexDirection: 'row', borderColor: 'rgba(49, 111,244, 1)',
-				   width: '100%', justifyContent: 'space-around', alignItems: 'center' }}
-				  innerContainerStyles={{ justifyContent: 'space-between' }}/>
-			  
-		    </Card>
+       <Card containerStyle={{flexDirection: 'row', width: "100%",
+      height: 80, backgroundColor: 'rgba(49, 111,244, 1)',
+      borderColor: 'rgba(49, 111,244, 1)', justifyContent: 'space-around',
+      alignItems: 'stretch', marginTop: 0
+      }}> 
+          <Header
+          statusBarProps={{ barStyle: 'light-content' }}
+          centerComponent={{ text: 'FitQuest', style: { color: '#fff',
+          fontWeight: 'bold', fontSize: 30, marginTop: 0, marginBottom: 15 } }}
+          outerContainerStyles={{ backgroundColor: 'rgba(49, 111,244, 1)',
+           flexDirection: 'row', borderColor: 'rgba(49, 111,244, 1)',
+           width: '100%', justifyContent: 'space-around', alignItems: 'center' }}
+          innerContainerStyles={{ justifyContent: 'space-between' }}/>
+        
+        </Card>
 
-			
-				<ButtonGroup
-			      onPress={this.updateIndex}
-			      selectedIndex={selectedIndex}
-			      buttons={buttons}
-			      buttonStyle={{justifyContent: 'center', width: '100%'}}
-			      containerStyle={{height: 70, width: '100%',
-			      backgroundColor: 'rgba(49, 111,244, 1)',
-			      justifyContent: 'flex-start', marginTop: 0}}
-			      textStyle={{color: 'white', fontWeight: 'bold'}}
-			    />
+      
+        <ButtonGroup
+            onPress={this.updateIndex}
+            selectedIndex={selectedIndex}
+            buttons={buttons}
+            buttonStyle={{justifyContent: 'center', width: '100%'}}
+            containerStyle={{height: 70, width: '100%',
+            backgroundColor: 'rgba(49, 111,244, 1)',
+            justifyContent: 'flex-start', marginTop: 0, marginBottom: 0}}
+            textStyle={{color: 'white', fontWeight: 'bold'}}
+          />
   
+  <Card containerStyle={{flexDirection: 'row', width: "100%",
+      height: '75%', backgroundColor: 'white',
+      borderColor: 'rgba(49, 111,244, 1)', justifyContent: 'space-around',
+      alignItems: 'center', marginTop: 0, 
+      }}> 
+    <Card containerStyle={{flexDirection: 'row', width: "95%",
+        height: 175, backgroundColor: 'rgba(44, 244, 250, 1)',
+        borderColor: 'rgba(44, 244, 250, 1)', justifyContent: 'space-around',
+        alignItems: 'center', marginTop: 30
+        }}> 
+        <Icon
+  name='assignment' color='rgba(49, 111,244, 1)'/>
+        <Text style={styles.text}>{`Daily`}</Text>
 
-	     <Card containerStyle={{flexDirection: 'row', width: "100%",
-			height: 200, backgroundColor: 'rgba(44, 244, 250, 1)',
-			borderColor: 'rgba(44, 244, 250, 1)', justifyContent: 'space-around',
-			alignItems: 'stretch', marginTop: 10
-			}}> 
-			<Text style={styles.text}>{`<Daily>`}</Text>
+      </Card>
+         <Card containerStyle={{flexDirection: 'row', width: "95%",
+        height: 175, backgroundColor: 'rgba(44, 244, 250, 1)',
+        borderColor: 'rgba(44, 244, 250, 1)', justifyContent: 'space-around',
+        alignItems: 'center', marginTop: 15
+        }}> 
+        <Icon
+  name='stars' color='rgba(49, 111,244, 1)' />
+        <Text style={styles.text}>{`Public`}</Text>
 
-		</Card>
-	     <Card containerStyle={{flexDirection: 'row', width: "100%",
-			height: 200, backgroundColor: 'rgba(44, 244, 250, 1)',
-			borderColor: 'rgba(44, 244, 250, 1)', justifyContent: 'space-around',
-			alignItems: 'stretch', marginTop: 10
-			}}> 
-			<Text style={styles.text}>{`<Public>`}</Text>
+      </Card>
+         <Card containerStyle={{flexDirection: 'row', width: "95%",
+        height: 175, backgroundColor: 'rgba(44, 244, 250, 1)',
+        borderColor: 'rgba(44, 244, 250, 11)', justifyContent: 'space-around',
+        alignItems: 'center', marginTop: 15
+        }}> 
+        <Icon
+  name='explore' color='rgba(49, 111,244, 1)' />
+        <Text style={styles.text}>{`New`}</Text>
 
-		</Card>
-	     <Card containerStyle={{flexDirection: 'row', width: "100%",
-			height: 200, backgroundColor: 'rgba(44, 244, 250, 1)',
-			borderColor: 'rgba(44, 244, 250, 11)', justifyContent: 'space-around',
-			alignItems: 'stretch', marginTop: 10
-			}}> 
-			<Text style={styles.text}>{`<New>`}</Text>
-
-		</Card>
+      </Card>
+    </Card> 
+    
+         
 
       </View>
     );
@@ -107,7 +97,7 @@ updateIndex (selectedIndex) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgba(244, 49,229, 1)',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: "flex-start"
     
@@ -125,9 +115,12 @@ const styles = StyleSheet.create({
         marginBottom: 20
     },
     text: {
-        color: "rgba(244, 49,229, 1)",
+        color: 'rgba(49, 111,244, 1)',
         fontWeight: "bold",
-        fontSize: 20
+        fontSize: 30,
+        alignItems: 'center',
+        textAlign: 'justify'
+        
     },
     textHome: {
         color: "white",
